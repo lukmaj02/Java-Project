f566f4c40ddf7d68ead5471779952635b6c6deb0
Lukasz Majcher <lmajcher02@gmail.com>
2023-11-14 19:00:23 +0100

    Init commit

---
+++ src/test/java/com/Projekt/Bankomat/BankomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projekt.Bankomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
