166e944a9e184afb34994a2f5c18151305503ad9
Lukasz Majcher <lmajcher02@gmail.com>
2023-11-16 08:08:46 +0100

    Deleted przelew

--- src/main/java/com/Projekt/Bankomat/Repository/PrzelewRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Projekt.Bankomat.Repository;
-
-import com.Projekt.Bankomat.Models.Przelew;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrzelewRepo extends JpaRepository<Przelew,String> {
-}
