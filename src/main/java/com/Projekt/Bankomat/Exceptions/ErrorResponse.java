ed1bff16c6244b02704eb8fa1116ae3aa8725ac2
radoni02 <108626591+radoni02@users.noreply.github.com>
2023-11-17 15:39:43 +0100

    ErrorResponce and handler added

---
+++ src/main/java/com/Projekt/Bankomat/Exceptions/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.Projekt.Bankomat.Exceptions;
+
+public class ErrorResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
