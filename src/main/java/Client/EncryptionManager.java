8d3a03e771efc3919af21a888d0aa524892ba73b
Lukasz Majcher <lmajcher02@gmail.com>
2023-11-29 11:25:46 +0100

    Added encrypting communication between

---
+++ src/main/java/Client/EncryptionManager.java
@@ -0,0 +1,34 @@
+package Client;
+
+import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class EncryptionManager {
+
+    private static final String PUBLIC_KEY = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKYmNjmj9eQAvNFVVkZToJ4fMLmnUbRYEN8nM9lC8YNItdOd8d5h5FwfLv2iti5ky4TYZ+1a4ZQ0f/X3+fiFw2ECAwEAAQ==";
+    private PublicKey publicKey;
+
+    public String encode(byte[] data){
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    public byte[] decode(String data) throws  Exception {
+        return Base64.getDecoder().decode(data);
+    }
+
+    public void initFromString() throws Exception {
+        X509EncodedKeySpec keySpecPublic = new X509EncodedKeySpec(decode(PUBLIC_KEY));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        publicKey = keyFactory.generatePublic(keySpecPublic);
+    }
+    public String encrypt(String msg) throws Exception{
+        byte[] messageToBytes = msg.getBytes();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+        byte[] encryptedBytes = cipher.doFinal(messageToBytes);
+        return encode(encryptedBytes);
+    }
+}
